e5b0b53b41f14de85ce29bf0e331f60cbde180a3
Fernando Muñoz <fernando1986caballeria@gmail.com>
2025-07-22 21:35:32 -0500

    button modal

--- src/components/ModalTask.jsx
+++ src/components/ModalTask.jsx
@@ -6,7 +6,9 @@ import Modal from 'react-bootstrap/Modal';
 const ModalTask = ({ showModal, setShowModal }) => {
 
     const [infoTitle, setInfoTitle] = useState();
-    const [infoDescription, setInfoDescription ]= useState();
+    const [infoDescription, setInfoDescription] = useState();
+    const [prioridad, setPrioridad] = useState();
+    const [fechaLimite, setFechaLimite] = useState();
 
 
     if (!showModal) return null;
@@ -19,17 +21,17 @@ const ModalTask = ({ showModal, setShowModal }) => {
 
     };
 
-    const handleCreateTask = ()=>{
-        if(infoTitle != ''){
+    const handleCreateTask = () => {
+        if (infoTitle != '') {
             console.log(infoTitle);
             console.log(infoDescription);
             setInfoTitle('');
             setInfoDescription('');
 
             handleClose();
-        }else{
+        } else {
             console.log('El titulo no puede estar vacio');
-        }  
+        }
     }
 
     return (
@@ -44,7 +46,7 @@ const ModalTask = ({ showModal, setShowModal }) => {
                             <Form.Label>Título</Form.Label>
                             <Form.Control
                                 type="text"
-                                placeholder="Ingresa el titulo de la tarea"
+                                placeholder="Ingresa el título de la tarea"
                                 autoFocus
                                 value={infoTitle}
                                 onChange={e => setInfoTitle(e.target.value)}
@@ -58,18 +60,47 @@ const ModalTask = ({ showModal, setShowModal }) => {
                             <Form.Control
                                 as="textarea"
                                 rows={3}
-                                placeholder='Describe la tarea'
+                                placeholder='Describe la tarea...'
                                 value={infoDescription}
                                 onChange={e => setInfoDescription(e.target.value)}
                             />
                         </Form.Group>
+                        <div className='row'>
+                            <Form.Group className="mb-3 col-md-6" >
+                                <Form.Label>Prioridad</Form.Label>
+                                <Form.Select>
+                                    <option value="seleccionar">Seleccionar</option>
+                                    <option value="alta">Alta</option>
+                                    <option value="media">Media</option>
+                                    <option value="baja">baja</option>
+                                </Form.Select>
+
+                            </Form.Group>
+                            <Form.Group className="mb-3 col-md-6">
+                                <Form.Label>Fecha límite</Form.Label>
+                                <Form.Control type="date" >
+
+                                </Form.Control>
+                            </Form.Group>
+                        </div>
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Categoría</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="ej. Desarrollo, Diseño..."
+                                autoFocus
+                                value={infoTitle}
+                                onChange={e => setInfoTitle(e.target.value)}
+                            />
+                        </Form.Group>
                     </Form>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleCreateTask}>
+                <Modal.Footer className='d-flex justify-content-between'>
+                    <Button className='btn-lg px-5' variant="dark" onClick={handleCreateTask}>
                         Crear Tarea
                     </Button>
-                    <Button variant="secondary" onClick={handleClose}>
+                    <Button className='btn-lg' variant="outline-secondary" onClick={handleClose}>
                         Cancelar
                     </Button>
                 </Modal.Footer>
