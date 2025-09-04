f076ff8e3a4d3f504b9ada8ccb4e8ff85540e476
Fernando Muñoz <fernando1986caballeria@gmail.com>
2025-09-03 21:16:30 -0500

    rename import

--- src/components/modals/ModalClose.jsx
+++ src/components/modals/ModalClose.jsx
@@ -1,5 +1,5 @@
 import { Modal, Button } from 'react-bootstrap';
-import '../../styles/ModalClose.css';
+import '../../styles/Modalclose.css';
 
 const ModalClose = ({ show, handleClose }) => {
     return (
